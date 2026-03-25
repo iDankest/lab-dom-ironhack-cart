ca5978d3f212db827d69e4a98d6983b91a255a32
Kilian <dankestmirror@hotmail.es>
2026-03-25 17:20:26 +0000

    it 4, 5 Fumadote el 4 me costo mas que el 5 tiste

--- src/index.js
+++ src/index.js
@@ -35,15 +35,43 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+ const target = event.currentTarget;
+ // console.log('The target in remove is:', target);
   //... your code goes here
+  const tr = target.parentNode.parentNode  // subimos hasta el tr
+  const tbody = tr.parentNode  // subimos hasta el tbody
+
+  tbody.removeChild(tr)  // eliminamos el tr del tbody
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
+  const inputName = document.querySelector('[placeholder="Product Name"]').value
+  const inputPrice = document.querySelector('[placeholder="Product Price"]').value
+  const createEl = document.createElement('tr')
+  
+
+  createEl.className = 'product'
+  createEl.innerHTML = `
+  <td class="name">
+            <span>${inputName}</span>
+          </td>
+          <td class="price">$<span>${inputPrice}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>
+         
+  `
+  document.querySelector('tbody').appendChild(createEl)
+  
+  document.querySelector('[placeholder="Product Name"]').value = ""
+  document.querySelector('[placeholder="Product Price"]').value = 0
 }
 
 window.addEventListener('load', () => {
@@ -51,4 +79,10 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  let buton = document.querySelectorAll('.btn-remove')
+  buton.forEach(btn => {
+    btn.addEventListener('click', removeProduct);
+  })
+  const botonCreate = document.querySelector('#create')
+  botonCreate.addEventListener('click', createProduct)
 });
